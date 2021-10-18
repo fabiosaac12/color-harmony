7fefcbb3f861e17578a9d3e3424c31ddd4307cd4
Fabio Isaac <fabiosaac12@gmail.com>
2021-10-18 07:52:16 -0400

    feat: app name and id

--- ios/ColorHarmonyTests/ColorHarmonyTests.m
+++ ios/ColorHarmonyTests/ColorHarmonyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeArchitectureTSTests : XCTestCase
+@interface ColorHarmonyTests : XCTestCase
 
 @end
 
-@implementation ReactNativeArchitectureTSTests
+@implementation ColorHarmonyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
