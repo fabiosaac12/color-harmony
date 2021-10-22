ba5798f1d1ffbb08448a8d959bfaed5ea383fc1f
Fabio Isaac <fabiosaac12@gmail.com>
2021-10-22 08:24:18 -0400

    feat: splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { MainStackNavigator } from 'navigation/MainStackNavigator';
 import { LanguageProvider } from 'providers/Language';
@@ -6,19 +6,26 @@ import { LoaderProvider } from 'providers/Loader';
 import { ModalProvider } from 'providers/Modal';
 import { PermissionsProvider } from 'providers/Permissions';
 import { ThemeProvider } from 'providers/Theme';
+import SplashScreen from 'react-native-splash-screen';
 
-export const App = () => (
-  <SafeAreaProvider>
-    <LanguageProvider defaultLanguage="en">
-      <LoaderProvider>
-        <PermissionsProvider>
-          <ThemeProvider defaultTheme="light">
-            <ModalProvider>
-              <MainStackNavigator />
-            </ModalProvider>
-          </ThemeProvider>
-        </PermissionsProvider>
-      </LoaderProvider>
-    </LanguageProvider>
-  </SafeAreaProvider>
-);
+export const App = () => {
+  useEffect(() => {
+    setTimeout(() => SplashScreen.hide(), 1000);
+  }, []);
+
+  return (
+    <SafeAreaProvider>
+      <LanguageProvider defaultLanguage="en">
+        <LoaderProvider>
+          <PermissionsProvider>
+            <ThemeProvider defaultTheme="light">
+              <ModalProvider>
+                <MainStackNavigator />
+              </ModalProvider>
+            </ThemeProvider>
+          </PermissionsProvider>
+        </LoaderProvider>
+      </LanguageProvider>
+    </SafeAreaProvider>
+  );
+};
