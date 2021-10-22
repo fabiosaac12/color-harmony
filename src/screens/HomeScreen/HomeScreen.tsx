82f9ae0c376ec0fd0c7e32a29c885bb58598ae1e
Fabio Isaac <fabiosaac12@gmail.com>
2021-10-22 07:29:49 -0400

    feat: logo in home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,14 +1,15 @@
+import { images } from 'assets';
 import { ColorHarmony } from 'components/ColorHarmony';
 import { HSLInput } from 'components/HSLInput';
 import { useTheme } from 'providers/Theme';
 import React, { FC } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { withLayout } from '../../hoc';
 import { useStyles } from './HomeScreenStyles';
 
 export const HomeScreen: FC = withLayout(() => {
   const styles = useStyles();
-  const { generateTheme } = useTheme();
+  const { generateTheme, theme } = useTheme();
 
   return (
     <TouchableOpacity
@@ -17,6 +18,7 @@ export const HomeScreen: FC = withLayout(() => {
       style={styles.container}
     >
       <HSLInput />
+      <Image source={images.logo} style={styles.logo} />
       <ColorHarmony />
     </TouchableOpacity>
   );
--- src/screens/HomeScreen/HomeScreenStyles.ts
+++ src/screens/HomeScreen/HomeScreenStyles.ts
@@ -1,5 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import { makeStyles } from '../../providers/Theme';
+
+const window = Dimensions.get('window');
 
 export const useStyles = makeStyles((theme) =>
   StyleSheet.create({
@@ -7,6 +9,14 @@ export const useStyles = makeStyles((theme) =>
       paddingVertical: theme.spacing(2),
       flex: 1,
       justifyContent: 'space-between',
+    },
+    logo: {
+      width: window.width / 3,
+      height: window.width / 3,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      tintColor: theme.palette.text,
+      opacity: 0.1,
     },
   }),
 );
